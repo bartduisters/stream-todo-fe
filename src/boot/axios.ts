cb54471965685768e18c7c8f65625d743e9988ec
Bart Duisters <bartduisters@bartduisters.com>
2022-11-01 20:26:34 +0100

    hard coded production backend url

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:3033/api' });
+const api = axios.create({
+  baseURL: 'https://api.code-coaching.dev/api/todos/api',
+});
+
+/*
+ * Als de backend lokaal draait, gebruik dan deze baseURL:
+ */
+// const api = axios.create({ baseURL: 'http://localhost:3033/api' });
 
 export { api };
